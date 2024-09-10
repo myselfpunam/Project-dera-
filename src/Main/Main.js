2133b4069f7493d1a30a0cae5cd7e898dbf4aaa6
puna1175 <myselfpunam.bhuiyan@gmail.com>
2024-09-10 12:57:24 +0600

    footer added

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from '../component/Navbar/Navigation';
 import { Outlet } from 'react-router-dom';
+import Footer from '../component/Footer/Footer';
 
 const Main = () => {
   return (
     <div>
       <Navigation></Navigation>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,164 @@
+import React from 'react';
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBIcon,
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+const Footer = () => {
+  return (
+    <MDBFooter className='text-center' color='white' bgColor='dark'>
+      <MDBContainer className='p-4'>
+        <section className='mb-4'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum repellat quaerat
+            voluptatibus placeat nam, commodi optio pariatur est quia magnam eum harum corrupti dicta, aliquam
+            sequi voluptate quas.
+          </p>
+        </section>
+
+        <section className=''>
+          <MDBRow>
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'>Dera Project</h5>
+              <hr
+                className="mb-4 mt-0 d-inline-block mx-auto"
+                style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px' }}
+              />
+              <ul className='list-unstyled mb-0'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </MDBCol>
+
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'>Services</h5>
+              <hr
+                className="mb-4 mt-0 d-inline-block mx-auto"
+                style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px' }}
+              />
+              <ul className='list-unstyled mb-0'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </MDBCol>
+
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'>Usefull Link</h5>
+              <hr
+                className="mb-4 mt-0 d-inline-block mx-auto"
+                style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px' }}
+              />
+              <ul className='list-unstyled mb-0'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </MDBCol>
+
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'>Contact</h5>
+              <hr
+                className="mb-4 mt-0 d-inline-block mx-auto"
+                style={{ width: '60px', backgroundColor: '#7c4dff', height: '2px' }}
+              />
+
+              <ul className='list-unstyled mb-0'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </MDBCol>
+          </MDBRow>
+        </section>
+      </MDBContainer>
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2024 Dera Interior | All rights reserved
+      </div>
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        Design and Developed by
+        <a className='text-bold text-decoration-none' href='#' style={{ marginLeft: '8px', color:'#DDBD73' }}>
+        Bird's Nest Cloud
+      </a>
+      </div>
+      
+      
+    </MDBFooter>
+  );
+};
+
+export default Footer;
